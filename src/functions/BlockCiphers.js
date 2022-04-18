0e80efdf77af3f7202431e058b9426d839beb6d6
viktory4121 <medvedka4121@gmail.com>
2022-04-18 08:01:41 +0300

    fix

--- src/functions/BlockCiphers.js
+++ src/functions/BlockCiphers.js
@@ -2,70 +2,222 @@
 params = {message: исходное сообщение, key: массив из 8 четырёхзначных чисел}
 */
 
+//console.log(methods2({message: "hello", key:[1123,0350,6431,9202,7265,3304,7782,9955]}))
+
 function methods2(params){
     let rezult = BlockCipher(params);
     return rezult;
 }
 function BlockCipher(params){
-    let replacementMatrix = [ //таблица замен:
-    [1,7,14,13,0,5,8,3,4,15,10,6,9,12,11,2],
-    [8,14,2,5,6,9,1,12,15,4,11,0,13,10,3,7],
-    [5,13,15,6,9,2,12,10,11,7,8,1,4,3,14,0],
-    [7,15,5,10,8,1,6,13,0,9,3,14,11,4,2,12],
-    [12,8,2,1,13,4,15,6,7,0,10,5,3,14,9,11],
-    [11,3,5,8,2,15,10,13,14,1,7,4,12,9,6,0],
-    [6,8,2,3,9,10,5,12,1,14,4,7,11,13,0,15],
-    [12,4,6,2,10,5,11,9,14,8,13,7,0,3,15,1]];
+    let replacementMatrix = generateReplaceMatrix();
 
-    let N1, N2; //32-разрядные накопители
-    let CM1, CM2;
-    let R;      //32-разрядный регистр циклического сдвига
-    let T = [];
+    let T = [], T_new = [];
     let mess = params.message.split("");
     let binary_message = [];
     let big_binary_mess = "";
+    let encode_bin = [];
+    let count_bloks = 0;
 
-    for(let i = 0; i < mess.length; i++){
-        binary_message.push(mess[i].charCodeAt(i).toString(2).padStart(16, '0'));
+    for(let i = 0; i < mess.length; i++){ //перевод символов в двоичную систему ascii
+        binary_message.push(mess[i].charCodeAt(0).toString(2).padStart(16, '0'));
         big_binary_mess = big_binary_mess + binary_message[i];
     }
 
-    for(let i = 0; i < 64; i++){ //деление на блоки и на из правые и левые части
-        T.push({L: big_binary_mess.slice(i, i + 32), R: big_binary_mess.slice(32 + i, i + 64)});
+    for(let i = 0; i < big_binary_mess.length; i += 64){ //деление на блоки и на правые и левые части
+        T.push({L: big_binary_mess.slice(i, i + 32), R: big_binary_mess.slice(32 + i, i + 64).padStart(16, '0')});
+        count_bloks++;
     }
     
-
-
-    return {message: mess, binary_message: binary_message};
+    for(let i = 0; i < count_bloks; i++){
+        T_new.push(encoding_BC(T[i], params, count_bloks, replacementMatrix)); //функция возвращает массив символов
+        encode_bin.push(T_new.toString(2).padStart(16, '0'));
+    }
+    
+    return {message: mess, binary_message: binary_message, encode: T_new, encode_binary: encode_bin};
 }
-function encoding_BC(){
+function encoding_BC(t, params, count, table){
+    let T = [];
+    let T_rez;
+    let arr_T = [];
+    let rezult_str = [];
 
-}
-function decoding_BC(){
+    T.push(t); //0-е значение
 
+    for(let i = 1; i <= 32; i++){ //цикл зашифрования
+        if(i <= 24){
+            T.push({L: func(addition_2_32(T[i - 1].L, params.key[(i - 1) % 8]), table[i]) ^ T[i - 1].R, R: T[i - 1].L});
+
+        } else if(i > 24 && i <= 31) {
+            T.push({L: func(addition_2_32(T[i - 1].L, params.key[31 - i]), table[i]) ^ T[i - 1].R, R: T[i - 1].L});
+
+        } else if(i == 32) {
+            T_rez = {L: (T[i - 1].L).toString(2), R: (func(addition_2_32(T[i - 1].L, params.key[0]), table[i]) ^ T[i - 1].R).toString(2)};
+        }
+    }
+    
+    arr_T = T_rez.L.match(/.{1,16}/g);
+    
+    for(let i = 0; i < arr_T.length; i++){
+        //console.log(arr_T[i])
+        rezult_str.push(generateReplaceMatrix(parseInt(arr_T[i], 2)));
+    }
+    console.log(rezult_str)
+    
+    return arr_T;
 }
-function addition_2(a, b){
-    return a ^ b;
+function func(elem, table){
+    let bin_elem = elem.toString(2).padStart(32, '0').slice(0);
+    let arr_4elem = bin_elem.match(/.{4}/g);
+    let new_elem = "";
+    
+    
+    for(let i = 0; i < arr_4elem.length; i++){
+        for(let k = 0; k < table.length; k++){
+            if(k == Math.abs(parseInt(arr_4elem[i], 2))){
+                console.log(table[k].toString(2))
+                new_elem = new_elem + table[k].toString(2).padStart(4, '0');
+                break;
+            }
+        }
+    }
+    //чёт не то... есть числа больше 4 цифр
+    console.log(new_elem)
+    
+    return new_elem.padStart(32, "0");
 }
+function generateReplaceMatrix(){
+    let table = new Array(33);
+    var nums;
+    let i, j;
+    
+    for(let k = 0; k < 33; k++){
+        table[k] = new Array(16);
+        nums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+        i = nums.length;
+        let z = 0;
+        j = 0;
+        
+        while (i--) {
+            j = Math.floor(Math.random() * (i+1));
+            table[k][z] = nums[j];
+            nums.splice(j, 1);
+            z++;
+        }
+    }
+    
+    return table;
+}
+
 function addition_2_32(a, b){
-    let count = pow(2, 32);
-    let sum;
-    if(a + b < count){
-        sum = a + b;
-    } else {
-        sum = a + b - count;
-    }
-    return sum;
+    return ((a + b.toString(2).padStart(16, '0')) % (Math.pow(2, 32)));
 }
-function addition_2_32_1(a, b){
-    let count = pow(2, 32) - 1;
-    let sum;
-    if(a + b < count){
-        sum = a + b;
-    } else {
-        sum = a + b - count;
-    }
-    return sum;
-}
+function description_nonGraphic_symbols(num){
+    let rezult;
 
+    switch(num){
+        case 0:
+            rezult = 'NUL';
+            break;
+        case 1:
+            rezult = 'SON';
+            break;
+        case 2:
+            rezult = 'STX';
+            break;
+        case 3:
+            rezult = 'ETX';
+            break; 
+        case 4:
+            rezult = 'EOT';
+            break;
+        case 5:
+            rezult = 'ENQ';
+            break;
+        case 6:
+            rezult = 'ACK';
+            break;
+        case 7:
+            rezult = 'BEL';
+            break;
+        case 8:
+            rezult = 'BS';
+            break;
+        case 9:
+            rezult = 'TAB';
+            break;
+        case 10:
+            rezult = 'LF';
+            break;
+        case 11:
+            rezult = 'VT';
+            break; 
+        case 12:
+            rezult = 'FF';
+            break;
+        case 13:
+            rezult = 'CR';
+            break;
+        case 14:
+            rezult = 'SO';
+            break;
+        case 15:
+            rezult = 'SI';
+            break;
+        case 16:
+            rezult = 'DLE';
+            break;
+        case 17:
+            rezult = 'DC1';
+            break;
+        case 18:
+            rezult = 'DC2';
+            break;
+        case 19:
+            rezult = 'DC3';
+            break; 
+        case 20:
+            rezult = 'DC4';
+            break;
+        case 21:
+            rezult = 'NAK';
+            break;
+        case 22:
+            rezult = 'SYN';
+            break;
+        case 23:
+            rezult = 'ETB';
+            break;
+        case 24:
+            rezult = 'CAN';
+            break;
+        case 25:
+            rezult = 'EM';
+            break;
+        case 26:
+            rezult = 'SUB';
+            break;
+        case 27:
+            rezult = 'ESC';
+            break; 
+        case 28:
+            rezult = 'FS';
+            break;
+        case 29:
+            rezult = 'GS';
+            break;
+        case 30:
+            rezult = 'RS';
+            break;
+        case 31:
+            rezult = 'US';
+            break;
+        case 32:
+            rezult = '(sp)';
+            break;
+        default:
+            rezult = String.fromCharCode(num);
+    }
+
+    return rezult;
+}
 export default methods2;
